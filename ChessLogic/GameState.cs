b02c1180be04e7d58c9f32ea4203ec0b32abb6f9
Kazey0204 <minhanhnguyen130204@gmail.com>
2026-05-18 19:51:37 +0700

    generating moves p2

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -13,8 +13,25 @@ namespace ChessLogic
 
         public GameState(Board board, Player currentPlayer)
         {
-            Board = board;
             CurrentPlayer = currentPlayer;
+            Board = board;  
+        }
+
+        public IEnumerable<Move> LegalMovesForPiece(Position pos)
+        {
+            if (Board.IsEmpty(pos) || Board[pos].Color != CurrentPlayer)
+            {
+                return Enumerable.Empty<Move>();
+            }
+
+            Piece piece = Board[pos];
+            return piece.GetMoves(pos, Board);
+        }
+
+        public void MakeMove(Move move)
+        {
+            move.Execute(Board);
+            CurrentPlayer = CurrentPlayer.Opponent();
         }
     }
 }
--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -11,9 +11,19 @@ namespace ChessLogic
         public override PieceType Type => PieceType.Pawn;
         public override Player Color { get; }
 
+        private readonly Direction forward;
+
         public Pawn(Player color) 
         {
             Color = color;
+            if (color == Player.White)
+            {
+                forward = Direction.North;
+            }
+            else if (color == Player.Black)
+            {
+                forward = Direction.South;
+            }
         }
 
         public override Piece Copy()
@@ -21,6 +31,56 @@ namespace ChessLogic
             Pawn copy = new Pawn(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private static bool CanMoveTo(Position pos, Board board)
+        {
+            return Board.IsInside(pos) && board.IsEmpty(pos);
+        }
+
+        private bool CanCaptureAt(Position pos, Board board)
+        {
+            if (!Board.IsInside(pos) || board.IsEmpty(pos))
+            {
+                return false;
+            }
+
+            return board[pos].Color !=Color;
+        }
+
+        private IEnumerable<Move> ForwardMoves(Position from, Board board)
+        {
+            Position oneMovepos = from + forward;
+
+            if (CanMoveTo(oneMovepos, board))
+            {
+                yield return new NormalMove(from, oneMovepos);
+
+                Position twoMovespos = oneMovepos + forward;
+
+                if (!HasMoved && CanMoveTo(twoMovespos, board))
+                {
+                    yield return new NormalMove(from, twoMovespos);
+                }
+            }
+        }
+
+        private IEnumerable<Move> DiagonalMoves(Position from, Board board)
+        {
+            foreach (Direction dir in new Direction[] { Direction.West, Direction.East })
+            {
+                Position to = from + forward + dir;
+
+                if (CanCaptureAt(to, board))
+                {
+                    yield return new NormalMove(from, to);
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return ForwardMoves(from, board).Concat(DiagonalMoves(from, board));
         }
     }
 }
