a3abadb42c35d8483d2138f0e508890e9480badb
William-remake <td0311886@gmail.com>
2026-05-19 15:56:04 +0700

    add Detect Check & Legal Moves

--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -43,5 +43,14 @@ namespace ChessLogic
         {
             return dirs.SelectMany(dir => MovePositionInDir(from, board, dir));
         }
+
+        public virtual bool CanCaptureOpponentKing(Position from, Board board)
+        {
+            return GetMoves(from, board).Any(move =>
+            {
+                Piece piece = board[move.ToPos];
+                return piece != null && piece.Type == PieceType.King;
+            });
+        }
     }
 }
