2baf29609d87c398892a8bd4972047daf7b10103
William-remake <td0311886@gmail.com>
2026-05-20 10:53:34 +0700

    Complete GameOverMenu

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,59 @@ namespace ChessUI
     /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+        public event Action<Option> OptionSelected;
+
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReaSonText(result.Reason, gameState.CurrentPlayer);
+        }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "wHITE WINS!",
+                Player.Black => "BLACK WINS!",
+                _ => "IT'S A DRAW",
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            { 
+                Player.White => "WHITE",
+                Player.Black => "BLACK",
+                _ => "",
+            };
+        }
+
+        private static string GetReaSonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Stalemate => $"STALEMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.Checkmate => $"CHECKMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.FiftyMoveRule => "FIFTY-MOVE RULE",
+                EndReason.InsufficientMaterial => "INSUFFICIENT MATERIAL",
+                EndReason.ThreefoldRepetion => "THREEFOLD REPETION",
+                _ => "",
+
+            };
         }
 
         private void Restart_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Restart);
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
-        {
-
+        {   
+            OptionSelected?.Invoke(Option.Exit);
         }
     }
 }
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -69,6 +69,11 @@ namespace ChessUI
 
         private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if(IsMenuOnSreen())
+            {
+                return;
+            }
+
             Point point = e.GetPosition(BoardGrid);
             Position pos = ToSquarePosition(point);
 
@@ -118,6 +123,11 @@ namespace ChessUI
             gameState.MakeMove(move);
             DrawBoard(gameState.Board);
             SetCursor(gameState.CurrentPlayer);
+
+            if (gameState.IsGameOver())
+            {
+                ShowGameOver();
+            }
         }
 
         private void CacheMoves(IEnumerable<Move> moves)
@@ -158,6 +168,39 @@ namespace ChessUI
             {
                 Cursor = ChessCursors.BlackCursor;
             }
+        }
+
+        private bool IsMenuOnSreen()
+        {
+            return MenuContainer.Content != null;
+        }
+
+        private void ShowGameOver()
+        {
+            GameOverMenu gameOverMenu = new GameOverMenu(gameState);
+            MenuContainer.Content = gameOverMenu;
+
+            gameOverMenu.OptionSelected += option =>
+            {
+                if (option == Option.Restart)
+                {
+                    MenuContainer.Content = null;
+                    RestartGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            };
+        }
+
+        private void RestartGame()
+        {
+            HideHighlights();
+            moveCache.Clear();
+            gameState = new GameState(Board.Initial(), Player.White);
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
         }
     }
 }
