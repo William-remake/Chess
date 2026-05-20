338af065c1d04d5635e24d0712b9d9f3929c1d34
Kazey0204 <minhanhnguyen130204@gmail.com>
2026-05-20 18:50:24 +0700

    50 move rule & threefold repetition p1

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -12,6 +12,8 @@ namespace ChessLogic
         public Player CurrentPlayer { get; private set; }
         public Result Result { get; private set; } = null;
 
+        private int noCaptureOrPawnMoves = 0;
+
         public GameState(Board board, Player currentPlayer)
         {
             CurrentPlayer = currentPlayer;
@@ -33,8 +35,18 @@ namespace ChessLogic
         public void MakeMove(Move move)
         {
             Board.SetPawnSkipPosition(CurrentPlayer, null);
-            move.Execute(Board);
-            CurrentPlayer = CurrentPlayer.Opponent();
+            bool captureOrPawn = move.Execute(Board);
+
+            if (captureOrPawn)
+            {
+                noCaptureOrPawnMoves = 0;
+            }
+            else
+            {
+                noCaptureOrPawnMoves++;
+            }
+
+                CurrentPlayer = CurrentPlayer.Opponent();
             CheckForGameOver();
         }
 
@@ -66,11 +78,21 @@ namespace ChessLogic
             {
                 Result = Result.Draw(EndReason.InsufficientMaterial);
             }
+            else if (FiftyMoveRule())
+            {
+                Result = Result.Draw(EndReason.FiftyMoveRule);
+            }
         }
 
         public bool IsGameOver()
         {
             return Result != null;
+        }
+
+        private bool FiftyMoveRule()
+        {
+            int fullMMoves = noCaptureOrPawnMoves / 2;
+            return fullMMoves == 50;
         }
     }
 }
--- ChessLogic/Moves/EnPassant.cs
+++ ChessLogic/Moves/EnPassant.cs
@@ -20,10 +20,12 @@ namespace ChessLogic
             ToPos = to;
             capturePos = new Position(from.Row, to.Column);
         }
-        public override void Execute(Board board)
+        public override bool Execute(Board board)
         {
             new NormalMove(FromPos, ToPos).Execute(board);
             board[capturePos] = null;
+
+            return true;
         }
     }
 }
